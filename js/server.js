0b8a0cf960b38533fb1910b6473925b447f7cf68
rizkinabil <rizki.nbl123@gmail.com>
2021-04-22 11:48:26 +0700

    adding new file

--- js/server.js
+++ js/server.js
@@ -1,47 +1,47 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-// middleware
-app.use(express.static("public"));
-app.use(express.json());
+// // middleware
+// app.use(express.static("public"));
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-   res.sendFile(__dirname + "/public/index.html");
-});
+// app.get("/", (req, res) => {
+//    res.sendFile(__dirname + "/public/index.html");
+// });
 
-app.post("/", (req, res) => {
-   console.log(req.body);
+// app.post("/", (req, res) => {
+//    console.log(req.body);
 
-   const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-         user: "rizukinbr@gmail.com",
-         pass: "wR3ck1e$$",
-      },
-   });
+//    const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//          user: "rizukinbr@gmail.com",
+//          pass: "wR3ck1e$$",
+//       },
+//    });
 
-   const mailOption = {
-      form: req.body.email,
-      to: "rizukinbr@gmail.com",
-      subject: `Message from ${req.body.email}: ${req.body.subject}`,
-      text: req.body.message,
-   };
+//    const mailOption = {
+//       form: req.body.email,
+//       to: "rizukinbr@gmail.com",
+//       subject: `Message from ${req.body.email}: ${req.body.subject}`,
+//       text: req.body.message,
+//    };
 
-   transporter.sendMail(mailOption, (error, info) => {
-      if (error) {
-         console.log(error);
-         res.send("error");
-      } else {
-         console.log("Email sent: " + info.response);
-         res.send("sucsess");
-      }
-   });
-});
+//    transporter.sendMail(mailOption, (error, info) => {
+//       if (error) {
+//          console.log(error);
+//          res.send("error");
+//       } else {
+//          console.log("Email sent: " + info.response);
+//          res.send("sucsess");
+//       }
+//    });
+// });
 
-app.listen(PORT, () => {
-   console.log(`server running on PORT ${PORT}`);
-});
+// app.listen(PORT, () => {
+//    console.log(`server running on PORT ${PORT}`);
+// });
